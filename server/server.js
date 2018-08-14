214e7fea79ae783035534d863cee3ce97057c20d
Bala <bala_aryal123@hotmail.com>
2018-08-14 15:06:10 -0400

    Added GET players method

--- server/server.js
+++ server/server.js
@@ -6,6 +6,24 @@ var {mongoose} = require('./../database/mongoose.js');
 const {Player} = require('./../database/models/player.js');
 const fillPlayers = require('./../database/initialize_database/fill_players.js').Player;
 
+app.use(bodyParser.json());
+
+app.get('/players', (req, res) => {
+  Player.find().then((players) => {
+    res.send({
+      status: 200,
+      playersCount: players.length,
+      players: players
+    });
+  }, (err) => {
+    res.status(400).send({
+      status: 400,
+      errorMessage: 'Unable to return data',
+      errorDetail: `Error: ${err}`
+    });
+  } );
+});
+
 fillPlayers.addPlayers();
 var app = express();
 var port = process.env.PORT || 3000;
