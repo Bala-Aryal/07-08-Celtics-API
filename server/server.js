a3200043e8f50408d86f95b2fb2865714afcd08e
Bala <bala_aryal123@hotmail.com>
2018-08-14 15:03:46 -0400

    Testing database

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./../database/mongoose.js');
 const {Player} = require('./../database/models/player.js');
+const fillPlayers = require('./../database/initialize_database/fill_players.js').Player;
 
+fillPlayers.addPlayers();
 var app = express();
 var port = process.env.PORT || 3000;
  app.listen(port,() => {
