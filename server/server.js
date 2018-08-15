e52dfa804152afb7cdfcdd54fe75748da23df282
Bala <bala_aryal123@hotmail.com>
2018-08-15 13:39:05 -0400

    Readding all results

--- server/server.js
+++ server/server.js
@@ -15,112 +15,112 @@ var port = process.env.PORT || 3000;
 
 // fillPlayers.addPlayers();
 
-// fillGames.addResults();
+fillGames.addResults();
 //
 // fillGeneral.addGeneral();
 
 app.use(bodyParser.json());
 
-// app.get('/players', (req, res) => {
-//   Player.find().then((players) => {
-//     res.send({
-//       status: 200,
-//       playersCount: players.length,
-//       players: players});
-//   }, (err) => {
-//     res.status(400).send({
-//       status: 400,
-//       errorMessage: 'Unable to return data',
-//       errorDetail: `Error: ${err}`
-//     });
-//   });
-// });
-//
-// app.get('/players/:name', (req, res) => {
-//   var playerName = req.params.name.toLowerCase();
-//
-//   Player.findOne({popularName: playerName}).then((player) => {
-//     if(!player)
-//     {
-//       res.status(404).send({
-//         status: 404,
-//         errorMessage: 'Could not find specified player in database',
-//         errorDetail: 'No player in the databse matches with the name you provided. Please make sure you are passing in the player\'s popular name as the argument'
-//       });
-//
-//       return;
-//     }
-//     res.send({
-//       status: 200,
-//       player: player
-//     });
-//   },(err) => {
-//     res.status(400).send({
-//       status: 400,
-//       errorMessage: 'Unable to return data',
-//       errorDetail: `Errro: ${err}`
-//     });
-//   });
-// });
-//
-// app.get('/results', (req, res) => {
-//   Game.find().then((games) => {
-//     res.send({
-//       status: 200,
-//       resultsCount: games.length,
-//       results: games
-//     });
-//   }, (err) => {
-//     res.status(400).send({
-//       status: 400,
-//       errorMessage: 'Unable to return data',
-//       errorDetail: `Error: ${err}`
-//     });
-//   });
-// });
-//
-// app.get('/results/:competition', (req, res) => {
-//   var competition = req.params.competition.toLowerCase();
-//
-//   Game.find({competition: `${competition}`}).then((Games) => {
-//     if(!Games)
-//     {
-//       res.status(404).send({
-//         status: 404,
-//         errorMessage: 'Could not find speicifed result(s) in database',
-//         errorDetail: `No games in the database with speicifed search ${competition}`
-//       });
-//
-//       return;
-//     }res.send({
-//       status: 200,
-//       resultsCount: Games.length,
-//       results: Games
-//     });
-//
-//   }, (err) => {
-//     res.status(400).send({
-//       status: 400,
-//       errorMessage: 'Unable to return data',
-//       errorDetail: `Error: ${err}`
-//     });
-//   });
-// });
-//
-// app.get('/general', (req, res) => {
-//   General.find().then((doc) => {
-//     res.send({
-//       status: 200,
-//       clubInfo: doc
-//     });
-//   }, (err) => {
-//     res.status(400).send({
-//       status: 400,
-//       errorMessage: 'Unable to retrieve general information',
-//       errorDetail: `Error: ${err}`
-//     });
-//   });
-//  });
+app.get('/players', (req, res) => {
+  Player.find().then((players) => {
+    res.send({
+      status: 200,
+      playersCount: players.length,
+      players: players});
+  }, (err) => {
+    res.status(400).send({
+      status: 400,
+      errorMessage: 'Unable to return data',
+      errorDetail: `Error: ${err}`
+    });
+  });
+});
+
+app.get('/players/:name', (req, res) => {
+  var playerName = req.params.name.toLowerCase();
+
+  Player.findOne({popularName: playerName}).then((player) => {
+    if(!player)
+    {
+      res.status(404).send({
+        status: 404,
+        errorMessage: 'Could not find specified player in database',
+        errorDetail: 'No player in the databse matches with the name you provided. Please make sure you are passing in the player\'s popular name as the argument'
+      });
+
+      return;
+    }
+    res.send({
+      status: 200,
+      player: player
+    });
+  },(err) => {
+    res.status(400).send({
+      status: 400,
+      errorMessage: 'Unable to return data',
+      errorDetail: `Errro: ${err}`
+    });
+  });
+});
+
+app.get('/results', (req, res) => {
+  Game.find().then((games) => {
+    res.send({
+      status: 200,
+      resultsCount: games.length,
+      results: games
+    });
+  }, (err) => {
+    res.status(400).send({
+      status: 400,
+      errorMessage: 'Unable to return data',
+      errorDetail: `Error: ${err}`
+    });
+  });
+});
+
+app.get('/results/:competition', (req, res) => {
+  var competition = req.params.competition.toLowerCase();
+
+  Game.find({competition: `${competition}`}).then((Games) => {
+    if(!Games)
+    {
+      res.status(404).send({
+        status: 404,
+        errorMessage: 'Could not find speicifed result(s) in database',
+        errorDetail: `No games in the database with speicifed search ${competition}`
+      });
+
+      return;
+    }res.send({
+      status: 200,
+      resultsCount: Games.length,
+      results: Games
+    });
+
+  }, (err) => {
+    res.status(400).send({
+      status: 400,
+      errorMessage: 'Unable to return data',
+      errorDetail: `Error: ${err}`
+    });
+  });
+});
+
+app.get('/general', (req, res) => {
+  General.find().then((doc) => {
+    res.send({
+      status: 200,
+      clubInfo: doc
+    });
+  }, (err) => {
+    res.status(400).send({
+      status: 400,
+      errorMessage: 'Unable to retrieve general information',
+      errorDetail: `Error: ${err}`
+    });
+  });
+ });
 
 // //Use this route to delete duplicates in players collection
 
@@ -128,9 +128,9 @@ app.use(bodyParser.json());
   //   console.log(result);
   // });
 //
-Game.remove({}).then((result) => {
-  console.log(result);
-});
+// Game.remove({}).then((result) => {
+//   console.log(result);
+// });
 
 
  app.listen(port,() => {
